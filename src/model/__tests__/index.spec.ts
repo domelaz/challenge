68af7a0c7cf0903ecac79f97563ffbb5c70bf13a
Антон Домолазов <anton@domelaz.ru>
2017-01-04 14:25:12 +0300

    Try add vertice to graph

--- src/model/__tests__/index.spec.ts
+++ src/model/__tests__/index.spec.ts
@@ -1,12 +1,17 @@
 /**
  * Tests stuff
  */
-import { expect } from "chai";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
 
 /**
  * Code under test
  */
 import Model from "../";
+import Vertice from "../vertice";
 
 describe("Graph", function() {
   it("Model class", () => {
@@ -15,5 +20,22 @@ describe("Graph", function() {
     expect(model).to.respondTo("remove");
     expect(model).to.respondTo("getRoute");
     expect(model).to.have.property("vertices");
+  });
+
+  it("Add standalone vertice to graph", () => {
+    const model = new Model();
+    const vertice = new Vertice();
+    return Promise.all([
+      expect(vertice.edges).to.be.undefined,
+      expect(model.add(vertice).then(m => m.vertices.length)).eventually.equals(1),
+    ]);
+  });
+
+  it("Add vertice to graph as array", () => {
+    const model = new Model();
+    const vertice = new Vertice();
+    return Promise.all([
+      expect(model.add([vertice]).then(m => m.vertices.length)).eventually.equals(1),
+    ]);
   });
 });
