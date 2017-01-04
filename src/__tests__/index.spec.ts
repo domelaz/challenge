3cfb003f6821ad5a2f46ef6d39cecc9843cbe37c
Антон Домолазов <anton@domelaz.ru>
2017-01-04 11:33:49 +0300

    Add entry point

---
+++ src/__tests__/index.spec.ts
@@ -0,0 +1,15 @@
+/**
+ * Tests stuff
+ */
+import { expect } from "chai";
+
+/**
+ * Code under test
+ */
+import Subject from "../";
+
+describe("Launch test", function() {
+  it("All be ok", () => {
+    expect(Subject).to.be.ok;
+  });
+});
