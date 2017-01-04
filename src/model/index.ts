fb6734efddb51b27fd3a0d550d10de076c1b59e2
Антон Домолазов <anton@domelaz.ru>
2017-01-04 14:17:41 +0300

    Green-line Model class implementation

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,1 +1,21 @@
-export default <Elje.Model>true;
+class Model implements Elje.Model {
+  public vertices: Elje.Vertice[];
+
+  constructor() {
+    this.vertices = [];
+  }
+
+  add(node: Elje.NodeType | Elje.NodeType[]) {
+    return Promise.resolve(this);
+  }
+
+  remove(node: Elje.NodeType | Elje.NodeType[]) {
+    return Promise.resolve(this);
+  }
+
+  getRoute(node1: Elje.Vertice, node2: Elje.Vertice) {
+    return Promise.resolve([]);
+  }
+}
+
+export default Model;
