d4f975959cea4e5ae7de8368c701253e80a884e7
Антон Домолазов <anton@domelaz.ru>
2017-01-04 11:23:07 +0300

    Typings added

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/chai-as-promised/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/karma/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/promises-a-plus/index.d.ts" />
+/// <reference path="modules/log4js/index.d.ts" />
