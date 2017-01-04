cf8791a3aa80d02531dec4a8b41418c49153c0c7
Антон Домолазов <anton@domelaz.ru>
2017-01-04 14:16:07 +0300

    Model blueprint draft

--- src/interfaces/model.d.ts
+++ src/interfaces/model.d.ts
@@ -1,4 +1,33 @@
 declare namespace Elje {
+
+  interface Node<T> {
+    type: "vertice" | "edge";
+  }
+
+  interface Edge extends Node<Edge> {
+    vertices: [Vertice, Vertice];
+    weight: number;
+  }
+
+  interface Location {
+    x: number;
+    y: number;
+  }
+
+  interface Vertice extends Node<Vertice> {
+    cost?: number;
+    edges: Set<Edge>;
+    location: Location;
+    name?: string;
+  }
+
+  type NodeType = Edge | Vertice;
+
   interface Model {
+    edges: Edge[];
+    vertices: Vertice[];
+    add(node: NodeType | NodeType[]): Promise<Model>;
+    getRoute(start: Vertice, finish: Vertice): Promise<Edge[]>;
+    remove(node: NodeType | NodeType[]): Promise<Model>;
   }
 }
