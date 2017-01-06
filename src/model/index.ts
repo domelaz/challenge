94e5ef83093c0178a6ac8c6a41ad6aef7ea558b7
Антон Домолазов <anton@domelaz.ru>
2017-01-06 11:17:01 +0300

    Green-line Edge class implementation

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,8 +1,10 @@
 class Model implements Elje.Model {
+  public edges: Elje.Edge[];
   public vertices: Elje.Vertice[];
 
   constructor() {
     this.vertices = [];
+    this.edges = [];
   }
 
   protected normalizeToArray<T>(arg: T | T[]): T[] {
@@ -11,6 +13,10 @@ class Model implements Elje.Model {
 
   protected isVertices(nodes: Array<Elje.NodeType>): nodes is Elje.Vertice[] {
     return nodes[0].type === "vertice";
+  }
+
+  protected isEdges(nodes: Array<Elje.NodeType>): nodes is Elje.Edge[] {
+    return nodes[0].type === "edge";
   }
 
   add(node: Elje.NodeType | Elje.NodeType[]) {
@@ -24,6 +30,12 @@ class Model implements Elje.Model {
       const existing = this.vertices;
       const arrived = _nodes.filter( n => existing.indexOf(n) === -1 );
       this.vertices = existing.concat(arrived);
+    }
+
+    if (this.isEdges(_nodes)) {
+      const existing = this.edges;
+      const arrived = _nodes.filter( n => existing.indexOf(n) === -1 );
+      this.edges = existing.concat(arrived);
     }
 
     return Promise.resolve(this);
