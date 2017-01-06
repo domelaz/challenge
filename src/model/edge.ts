99cbbf96b519852e892ded11221adc5a859481cf
Антон Домолазов <anton@domelaz.ru>
2017-01-06 11:21:01 +0300

    Add edge to graph

--- src/model/edge.ts
+++ src/model/edge.ts
@@ -2,8 +2,17 @@ class Edge implements Elje.Edge {
   public readonly vertices;
   public readonly type = "edge";
 
+  protected hookParent(node: Elje.Vertice) {
+    if (Array.isArray(node.edges)) {
+      node.edges.push(this);
+    } else {
+      node.edges = [ this ];
+    }
+  }
+
   constructor (node1: Elje.Vertice, node2: Elje.Vertice) {
     this.vertices = [node1, node2];
+    this.vertices.forEach(this.hookParent, this);
   }
 
   calcWeight() {
