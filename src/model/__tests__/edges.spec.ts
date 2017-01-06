e83a129799538e61b8a7b006022ddb7644c3deb4
Антон Домолазов <anton@domelaz.ru>
2017-01-06 11:16:10 +0300

    Edge class test

---
+++ src/model/__tests__/edges.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * Tests stuff
+ */
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+
+const expect = chai.expect;
+chai.use(chaiAsPromised);
+
+/**
+ * Code under test
+ */
+import Model from "../";
+import Vertice from "../vertice";
+import Edge from "../edge";
+
+describe("Edge", function() {
+  it("Edge class", () => {
+    const node1 = new Vertice({x:0, y:0});
+    const node2 = new Vertice({x:2, y:2});
+    const edge = new Edge(node1, node2);
+    expect(edge).to.have.property("vertices");
+  });
+});
