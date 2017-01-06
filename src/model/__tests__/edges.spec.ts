23ac78a8990c7a413b829ea3096b35cbec281ae2
Антон Домолазов <anton@domelaz.ru>
2017-01-06 13:09:23 +0300

    Calculate Edge weight

--- src/model/__tests__/edges.spec.ts
+++ src/model/__tests__/edges.spec.ts
@@ -24,13 +24,13 @@ describe("Edge", function() {
 
   it("Add edge to graph", () => {
     const model = new Model();
-    const node1 = new Vertice({x:0, y:0});
-    const node2 = new Vertice({x:2, y:2});
+    const node1 = new Vertice({x:7, y:-2});
+    const node2 = new Vertice({x:-2, y:5});
     const edge = new Edge(node1, node2);
     return Promise.all([
       expect(model.add(edge).then(() => node1.edges[0])).eventually.equal(edge),
       expect(model.add(edge).then(() => node2.edges[0])).eventually.equal(edge),
-      expect(model.add(edge).then(() => edge.weight)).eventually.equal(42),
+      expect(model.add(edge).then(() => edge.weight)).eventually.be.closeTo(11.4, 11.4),
       expect(model.add(edge).then(m => m.edges.length)).eventually.equal(1),
     ]);
   });
--- src/model/edge.ts
+++ src/model/edge.ts
@@ -1,5 +1,5 @@
 class Edge implements Elje.Edge {
-  public readonly vertices;
+  public readonly vertices: [Elje.Vertice, Elje.Vertice];
   public readonly weight: number;
   public readonly type = "edge";
 
@@ -18,7 +18,12 @@ class Edge implements Elje.Edge {
   }
 
   protected calcWeight() {
-    return 42;
+    const [ node1, node2 ] = this.vertices;
+    const xy = [
+      node2.location.x - node1.location.x,
+      node2.location.y - node1.location.y,
+    ];
+    return Math.sqrt(Math.pow(xy[0], 2) + Math.pow(xy[1], 2));
   }
 }
 
