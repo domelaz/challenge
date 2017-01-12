19ba8ce8004cbbcdb4a4ad83b46942c9cb6f6f79
Антон Домолазов <anton@domelaz.ru>
2017-01-12 14:16:50 +0300

    Some lodash-like array helpers functions added

--- src/model/index.ts
+++ src/model/index.ts
@@ -5,6 +5,37 @@ class Model implements Elje.Model {
     this.vertices = [];
   }
 
+  /**
+   * Returns two arrays difference
+   */
+  protected diff<T, U extends T>(base: T[], addenum: U[]): T[] {
+    return addenum.filter(a => base.indexOf(a) === -1);
+  }
+
+  /**
+   * Returns two arrays intersection
+   */
+  protected intersect<T, U extends T>(base: T[], addenum: U[]): T[] {
+    return addenum.filter(a => base.indexOf(a) !== -1);
+  }
+
+  /**
+   * Merges second array into first
+   * Mutates first array
+   */
+  protected merge<T, U extends T>(base: T[], addenum: U[]) {
+    const newbies = this.diff(base, addenum);
+    base.splice(-1, 0, ...newbies);
+    return base;
+  }
+
+  /**
+   * Cast "maybe array" to array
+   */
+  protected arrize<T>(arg: T | T[]): T[] {
+    return Array.isArray(arg) ? arg : [arg];
+  }
+
   public add(node: Elje.NodeType | Elje.NodeType[]) {
     return Promise.resolve(this);
   }
